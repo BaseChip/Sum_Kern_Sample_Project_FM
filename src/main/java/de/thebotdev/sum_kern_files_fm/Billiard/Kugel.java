7da435d20ca28e43daa50db3c71d73d184879262
BaseChip <info@falkmichaelis.eu>
2019-01-12 11:27:57 +0100

    mehrere Kugel

--- src/main/java/de/thebotdev/sum_kern_files_fm/Billiard/Kugel.java
+++ src/main/java/de/thebotdev/sum_kern_files_fm/Billiard/Kugel.java
@@ -1,14 +1,19 @@
 package de.thebotdev.sum_kern_files_fm.Billiard;
 import sum.kern.*;
+import sum.werkzeuge.*;
 
 public class Kugel {
     private Buntstift meinStift;
+    private Rechner rechner;
     private double radius;
+    private int color;
 
     public Kugel(int y, int x, double radius){
         meinStift = new Buntstift();
+        rechner = new Rechner();
         meinStift.bewegeBis(x, y);
         this.radius = radius;
+        this.color = rechner.ganzeZufallsZahl(1, 12);
     }
 
     public void bewege(){
@@ -19,6 +24,7 @@ public class Kugel {
         this.zeichne();
     }
     public void zeichne(){
+        meinStift.setzeFarbe(color);
         meinStift.setzeFuellmuster(1);
         meinStift.zeichneKreis(radius);
     }
--- src/main/java/de/thebotdev/sum_kern_files_fm/Billiard/Main.java
+++ src/main/java/de/thebotdev/sum_kern_files_fm/Billiard/Main.java
@@ -5,6 +5,9 @@ import sum.kern.Bildschirm;
 public class Main {
     public Bildschirm derBildschirm = new Bildschirm(1200, 800);
     Kugel Kugel1 = new Kugel(100, 500, 15);
+    Kugel Kugel2 = new Kugel(200, 500, 15);
+    Kugel Kugel3 = new Kugel(300, 500, 15);
+    Kugel Kugel4 = new Kugel(400, 500, 15);
 
 
     public static void main(String[] args) {
@@ -17,6 +20,9 @@ public class Main {
     private void zeichne_kugeln(){
         while (true){
             Kugel1.bewege();
+            Kugel2.bewege();
+            Kugel3.bewege();
+            Kugel4.bewege();
         }
     }
 }
